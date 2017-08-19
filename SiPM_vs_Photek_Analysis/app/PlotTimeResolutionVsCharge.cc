3d1b0e9e4310987035329974d4dee629da3e432b
cmorgoth <cristian.morgoth@gmail.com>
2017-08-19 14:55:10 -0700

    add more data from 373nm laser

--- SiPM_vs_Photek_Analysis/app/PlotTimeResolutionVsCharge.cc
+++ SiPM_vs_Photek_Analysis/app/PlotTimeResolutionVsCharge.cc
@@ -33,7 +33,7 @@ TString lumiText = "12.92 fb^{-1} (13 TeV)";
 
 int main ( int argc, char** argv )
 {
-  int n = 14;
+  //int n = 14;
 /*
   //  ND:         0p5,  0p8,   1p1,   1p4,   1p6,  1p8_01, 1p8_02, 1p8_03, 04, 05, 06 , 07, 08, 09, 10, 11, 12  
   //  x: charge y: deltaT  
@@ -43,14 +43,14 @@ int main ( int argc, char** argv )
   float erryTHM[] = { 0.06,  0.07,  0.10,  0.05,  0.09,  0.92,  0.37,  0.35,  0.37,  0.39,  0.35, 0.52,  0.44,  0.49, 0.62,  0.68,  0.83};
   float x[] =    {1.05, 0.89, 0.39, 0.16, 0.09, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12};
   float errx[] = {0.04, 0.06, 0.09, 0.06, 0.04, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00};
-*/
+
   //  ND:         1p8_01, 1p8_02, 1p8_03, 04, 05, 06 , 07, 08, 09,  10, 11, 12,  0p8, 0p5  
   //  x: charge y: deltaT  
   float yTHM[]    = {64.25, 61.94, 58.13, 48.13, 46.47, 44.92, 42.59, 41.22, 41.60, 41.01, 39.72, 39.17, 21.72, 18.87};
   float erryTHM[] = { 0.92,  0.37,  0.35,  0.37,  0.39,  0.35,  0.52,  0.44,  0.49,  0.62,  0.68,  0.83,  0.07, 0.06};
   float x[] =    {0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12, 0.89, 1.05};
   float errx[] = {0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.06, 0.04};
-/*
+
   int n = 6;
   //  ND:         0p5,  0p8,   1p1,   1p4,   1p6,  1p8
   //  x: charge y: deltaT  
@@ -60,8 +60,8 @@ int main ( int argc, char** argv )
   float erryTHM[] = { 0.06,  0.07,  0.10,  0.05,  0.09,  0.09};
   float x[]    = {1.05, 0.89, 0.39, 0.16, 0.09, 0.06};
   float errx[] = {0.04, 0.06, 0.09, 0.06, 0.04, 0.03};
-  
-  int n = 5;
+
+  int n = 9;
   //  ND:         0p5,  0p8,   1p1,   1p4,   1p6
   //  x: charge y: deltaT  
   float y[]    = {31.92, 36.35, 42.32, 60.91, 87.24};
@@ -70,16 +70,95 @@ int main ( int argc, char** argv )
   float erryTHM[] = { 0.06,  0.07,  0.10,  0.05,  0.06};
   float x[]    = {1.05, 0.88, 0.40, 0.17, 0.10};
   float errx[] = {0.04, 0.07, 0.10, 0.06, 0.05};
-
+  
   int n = 7;
   float y[]    = {9.77, 12.84, 19.82, 27.98, 41.24,63.64, 84.84};
   float erry[] = {0.06, 0.06, 0.09, 0.14, 0.20,0.31, 0.48};
   float x[]    = {5.50, 3.06, 1.32, 0.69, 0.32, 0.15, 0.07};
   float errx[] = {0.27, 0.23, 0.16, 0.12, 0.08, 0.06, 0.03};
+  
+  int n = 9;
+  float y[]    = {7.11, 8.15, 9.41, 11.81, 15.87, 19.49, 48.06, 64.65, 85.98};
+  float erry[] = {0.05, 0.06, 0.07, 0.08, 0.11, 0.14, 0.37, 0.52, 1.47};
+  float x[]    = {4.384, 2.742, 1.808, 0.9831, 0.5937, 0.3862, 0.1077, 0.06313, 0.04589};
+  float errx[] = {0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001};
 
-   TGraphErrors* g = new TGraphErrors(n,x,y,errx,erry);
+//Jiajing's measurements, no amp, Marcel's algorithm
+  int n2 = 7;
+  float y2[]    = {11.99, 14.21, 21.17, 29.68, 43.99, 63.85, 82.15};
+  float erry2[] = {0.05, 0.05, 0.09, 0.1, 0.15, 0.28, 0.62};
+  float x2[]    = {6.484, 3.653, 1.606, 0.854, 0.414, 0.1931, 0.0973};
+  float errx2[] = {0.001, 0.001, 0.001, 0.001, 0.0004, 0.0005, 0.0004};
+
+  /* int n2 = 6;
+  float y2[]    = {22.94, 30.05, 31.43, 44.37, 65.92, 95.0}; //22.36
+  float erry2[] = {0.05, 0.08, 0.11, 0.15, 0.25, 0.5}; //0.05
+  float x2[]    = {12.654, 6.956, 6.233, 2.991, 1.081, 0.1946}; //38.12
+  float errx2[] = {0.005, 0.004, 0.006, 0.004, 0.004, 0.003}; //0.01
+  for (int i = 0; i < n2; i++){x2[i] = x2[i] / 63.095;}
+  */
+//BTL Laser Study
+  /*
+  //373nm, constant 70V
+  int n = 7;
+  float y[] = {31.58, 31.43, 49.27, 52.71, 60.48, 98.29, 317.06}; //Time resolution
+  //float x[] = {0, 1.4, 40, 50, 70, 85, 90.5}; //Tune
+  float x[] = {0.1032, 0.07676, 0.05022, 0.04476, 0.03658, 0.02042, 0.01056}; //Amplitude
+
+  //373nm, constant 0 tune
+  int n2 = 8;
+  float y2[] = {215.9, 72.98, 53.49, 43.39, 31.58, 34.01, 32.94, 33.28}; //Time Resolution
+  // float x2[] = {66, 67, 68, 69, 70, 71, 72, 73}; //Voltage
+  float x2[] = {0.007656, 0.01656, 0.0284, 0.0423, 0.1032, 0.0793, 0.1016, 0.1232}; //Amplitude
+  
+  //405nm run 1, constant 0 tune
+  int n = 7;
+  float y[] = {33.36, 33.25, 33.18, 31.97, 29.44, 31.91, 31.06}; //Time resolution
+  //float x[] = {66, 67, 68, 69, 70, 71, 72, 73}; //Voltage
+  float x[] = {0.06743, 0.1338, 0.2048, 0.278, 0.904, 1.091, 1.258}; //Amplitude
+
+  //405nm run 1, constant 70V
+  int n2 = 6;
+  float y2[] = {29.44, 32.03, 33.61, 33.57, 46.22, 59.25}; //Time resolution
+  //float x2[] = {0, 40, 50, 70, 73.5, 74}; //Tune
+  float x2[] = {0.904, 0.2512, 0.2256, 0.09234, 0.01473, 0.009577}; //Amplitude
+
+  */
+  //405nm run 2, constant 0 tune
+  int n = 7;
+  float y[] = {34.71, 34.62, 38.64, 33.98, 35.1, 34.18, 33.4}; //Time resolution
+  //float x[] = {66, 67, 68, 69, 70, 71, 72}; //Voltage
+  float x[] = {0.0773, 0.1715, 0.2838, 0.4088, 0.5214, 0.6678, 0.8151}; //Amplitude
+
+  //405nm run 2, constant 70V
+  int n2 = 6;
+  float y2[] = {35.1, 32.2, 31.23, 40.08, 44.37, 54.7}; //Time resolution
+  //float x2[] = {0, 40, 50, 70, 73.5, 74}; //Tune
+  float x2[] = {0.5214, 0.305, 0.2613, 0.09602, 0.02159, 0.01331};
+  
+
+  /*
+  for(int i = 0; i < n; i++)
+  {
+    x[i] = x[i] / (.0973/63.095);
+    errx[i] = errx[i] / (.0973/63.095);
+  }
+
+  for(int i = 0; i < n2; i++)
+  {
+    x2[i] = x2[i] / (0.0118);
+    errx2[i] = errx2[i] / (0.0118);
+  }
+  */
+
+  // TGraphErrors* g = new TGraphErrors(n,x,y,errx,erry);
+  // TGraphErrors* g2 = new TGraphErrors(n2,x2,y2,errx2,erry2);
+   TGraphErrors* g = new TGraphErrors(n,x,y);
+   TGraphErrors* g2 = new TGraphErrors(n2,x2,y2);
 
    TCanvas* c = new TCanvas( "c", "c", 2119, 33, 800, 700 );
+   // c->SetLogx();
+   
    c->SetHighLightColor(2);
    c->SetFillColor(0);
    c->SetBorderMode(0);
@@ -90,17 +169,27 @@ int main ( int argc, char** argv )
    c->SetBottomMargin( bottomMargin );
    c->SetFrameBorderMode(0);
    c->SetFrameBorderMode(0);
-   TF1* f1Res = new TF1("f1Res","[0]/x + [1]/sqrt(x) + [2]",x[0],1.1);
+   // TF1* f1Res = new TF1("f1Res","[0]/x + [1]/sqrt(x) + [2]",x[0],x[n-1]);
+   // TF1* f1Res2 = new TF1("f1Res2","[0]/x + [1]/sqrt(x) + [2]",x2[0],x2[n2-1]);
+   //f1Res2->SetParLimits(0,0.0,1e12);
+   //f1Res2->SetParLimits(1,0.0,1e12);
+   //f1Res2->SetParLimits(2,0.0,1e12);
+   //f1Res->SetLineColor(1);
 
-   g->SetTitle(";charge [pC];time resolution [ps]");
+   // g->SetTitle(";Number of Photons;Time Resolution [ps]");
    //g->GetYaxis()->SetTitle("time resolution [ps]");
    //g->GetXaxis()->SetTitle("charge [pC]");
-   
-   g->Draw("AP");
+   g->GetYaxis()->SetRange(30, 55);
+
+   g2->Draw("AP");
+   g2->SetMarkerStyle(20);
+   g2->SetMarkerColor(kBlack);
+   g->Draw("P");
    g->SetMarkerStyle(20);
-   g->SetMarkerColor(kBlack);
-   
-   g->Fit(f1Res,"RE");
+   g->SetMarkerColor(kRed);   
+
+   // g->Fit(f1Res,"E");
+   //g2->Fit(f1Res2, "E");
    
    TLatex latex;
    latex.SetNDC();
@@ -108,12 +197,21 @@ int main ( int argc, char** argv )
    latex.SetTextColor(kBlack);    
    latex.SetTextFont(42);
    latex.SetTextAlign(31); 
-   latex.SetTextSize(0.03);    
-   latex.DrawLatex(0.87, 0.85, Form("#sigma_{t} = #frac{%.2f #pm %.2f [ps]}{Q (pC)} + #frac{%.2f #pm %.2f [ps]}{#sqrt{Q (pC)}} + %.2f #pm %.2f [ps]", f1Res->GetParameter(0), f1Res->GetParError(0), f1Res->GetParameter(1), f1Res->GetParError(1), f1Res->GetParameter(2), f1Res->GetParError(2) ));
-   c->SaveAs("deltaT_vs_Charge_SiPM_1x1mm.pdf");
-   c->SaveAs("deltaT_vs_Charge_SiPM_1x1mm.png");
-   c->SaveAs("deltaT_vs_Charge_SiPM_1x1mm.C");
-*/ 
+   latex.SetTextSize(0.02);    
+   //latex.DrawLatex(0.90, 0.85, Form("3x3mm SiPM: #sigma_{t} = #frac{%.2f #pm %.2f [ps]}{Q (pC)} + #frac{%.2f #pm %.2f [ps]}{#sqrt{Q (pC)}} + %.2f #pm %.2f [ps]", f1Res->GetParameter(0), f1Res->GetParError(0), f1Res->GetParameter(1), f1Res->GetParError(1), f1Res->GetParameter(2), f1Res->GetParError(2) ));
+   //latex.SetTextColor(kRed);
+    //latex.DrawLatex(0.90, 0.7, Form("1x1mm SiPM: #sigma_{t} = #frac{%.2f #pm %.2f [ps]}{Q (pC)} + #frac{%.2f #pm %.2f [ps]}{#sqrt{Q (pC)}} + %.2f #pm %.2f [ps]", f1Res2->GetParameter(0), f1Res2->GetParError(0), f1Res2->GetParameter(1), f1Res2->GetParError(1), f1Res2->GetParameter(2), f1Res2->GetParError(2) ));
+    // latex.DrawLatex(0.90, 0.85, Form("3x3mm SiPM: #sigma_{t} = #frac{%.2f #pm %.2f [ps]}{N} + #frac{%.2f #pm %.2f [ps]}{#sqrt{N}} + %.2f #pm %.2f [ps]", f1Res->GetParameter(0), f1Res->GetParError(0), f1Res->GetParameter(1), f1Res->GetParError(1), f1Res->GetParameter(2), f1Res->GetParError(2) ));
+   // latex.SetTextColor(kRed);
+   // latex.DrawLatex(0.90, 0.7, Form("1x1mm SiPM: #sigma_{t} = #frac{%.2f #pm %.2f [ps]}{N} + #frac{%.2f #pm %.2f [ps]}{#sqrt{N}} + %.2f #pm %.2f [ps]", f1Res2->GetParameter(0), f1Res2->GetParError(0), f1Res2->GetParameter(1), f1Res2->GetParError(1), f1Res2->GetParameter(2), f1Res2->GetParError(2) ));
+    c->SaveAs("405nm_amplitudes2.pdf");
+   // c->SaveAs("deltaT_vs_NPhotons_SiPM_3vs1logx.png");
+   // c->SaveAs("deltaT_vs_NPhotons_SiPM_3vs1logx.C");
+    // c->SaveAs("deltaT_vs_Charge_SiPM_3vs1.pdf");
+    //c->SaveAs("deltaT_vs_Charge_SiPM_3vs1.png");
+   //c->SaveAs("deltaT_vs_Charge_SiPM_3vs1.C");
+   
+   /*
    TGraphErrors* gr = new TGraphErrors(n,x,yTHM,errx,erryTHM);
 
    TCanvas* cr = new TCanvas( "cr", "cr", 2119, 33, 800, 700 );
@@ -152,6 +250,7 @@ int main ( int argc, char** argv )
    cr->SaveAs("deltaTTHM_vs_Charge_SiPM_1x1mm.pdf");
    cr->SaveAs("deltaTTHM_vs_Charge_SiPM_1x1mm.png");
    cr->SaveAs("deltaTTHM_vs_Charge_SiPM_1x1mm.C");
-
+   */
   return 0;
 }
+
